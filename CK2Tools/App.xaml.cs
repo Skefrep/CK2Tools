6602510cdee49959b5f8f54ed598c380bdc5bced
Skefrep <skefrep@cilg.org>
2017-01-14 18:51:36 +0100

    Removed useless do-while and managed the returned value on the application start

--- CK2Tools/App.xaml.cs
+++ CK2Tools/App.xaml.cs
@@ -11,22 +11,22 @@ namespace CK2Tools
             CurrentMod = new Mod();
 
             var status = AppSettings.IsAppDirDefined();
-
-            do
+            
+            switch (status)
             {
-                switch (status)
-                {
-                    case AppSettings.eSettingsDirStatus.OK:
+                case AppSettings.eSettingsDirStatus.OK:
+                    return;
+                case AppSettings.eSettingsDirStatus.NotFound:
+                    MessageBox.Show(CK2Tools.Properties.Resources.ConfigError_AppDirNotFound);
+                    goto case AppSettings.eSettingsDirStatus.NotDefined;
+                case AppSettings.eSettingsDirStatus.NotDefined:
+                    var window = new SetAppDirSimple();
+                    window.ShowDialog();
+
+                    if (window.Result == SetAppDirSimple.ReturnValue.OK)
                         return;
-                    case AppSettings.eSettingsDirStatus.NotFound:
-                        MessageBox.Show(CK2Tools.Properties.Resources.ConfigError_AppDirNotFound);
-                        goto case AppSettings.eSettingsDirStatus.NotDefined;
-                    case AppSettings.eSettingsDirStatus.NotDefined:
-                        var window = new SetAppDirSimple();
-                        window.ShowDialog();
-                        break;
-                } 
-            } while (true);
+                    break;
+            }
         }
 
         public Mod CurrentMod;
