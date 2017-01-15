8072ee9ced555e3558c43628647b7ad2f00f735b
Skefrep <skefrep@cilg.org>
2017-01-15 22:20:00 +0100

    Implemented the get method

--- CK2Tools/Settings/AppSettings.cs
+++ CK2Tools/Settings/AppSettings.cs
@@ -36,10 +36,28 @@ namespace CK2Tools
             }
         }
 
+        public struct KnownMod { public int id; public string name; public string path; };
+
         public static System.Collections.Generic.List<KnownMod> LastMods
         {
             get
             {
+                if (Properties.Settings.Default.LastMods == null)
+                    Properties.Settings.Default.LastMods = new System.Collections.Specialized.StringCollection();
+
+                if (Properties.Settings.Default.LastModsPaths == null)
+                    Properties.Settings.Default.LastModsPaths = new System.Collections.Specialized.StringCollection();
+
+                var mods = new System.Collections.Generic.List<KnownMod>();
+                for (int id = 0; id < Properties.Settings.Default.LastMods.Count; id++)
+                {
+                    var knownMod = new KnownMod();
+                    knownMod.id = id;
+                    knownMod.name = Properties.Settings.Default.LastMods[id];
+                    knownMod.path = Properties.Settings.Default.LastModsPaths[id];
+                    mods.Add(knownMod);
+                }
+                return mods;
             }
             set
             {
